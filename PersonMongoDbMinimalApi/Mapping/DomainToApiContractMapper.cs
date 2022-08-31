a12d90b8c1f1840a06b4887e770f4491fb11b38c
Rostislav Fedoriv <froizerner@gmail.com>
2022-08-31 12:10:41 +0300

    Added mapper

---
+++ PersonMongoDbMinimalApi/Mapping/DomainToApiContractMapper.cs
@@ -0,0 +1,34 @@
+﻿using PersonMongoDbMinimalApi.Contracts.Responses;
+using PersonMongoDbMinimalApi.Domain;
+
+namespace PersonMongoDbMinimalApi.Mapping;
+public static class DomainToApiContractMapper
+{
+    public static PersonResponse ToPersonResponse(this Person person)
+    {
+        return new PersonResponse
+        {
+            Id=person.Id,
+            FirstName=person.FirstName,
+            SecondName=person.SecondName,
+            Email=person.Email,
+            Telephone=person.Telephone,
+            Age=person.Age
+        };
+    }
+    public static GetPeopleResponse ToPeopleResponse(this IEnumerable<Person> people)
+    {
+        return new GetPeopleResponse
+        {
+            People = people.Select(p => new PersonResponse
+            {
+                Id = p.Id,
+                Age = p.Age,
+                FirstName = p.FirstName,
+                SecondName = p.SecondName,
+                Email = p.Email,
+                Telephone = p.Telephone
+            })
+        };
+    }
+}
