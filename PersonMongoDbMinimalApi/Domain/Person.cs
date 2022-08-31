b0d51fb400ab80c3a9a4eef697210e71b0ef8865
Rostislav Fedoriv <froizerner@gmail.com>
2022-08-31 11:12:17 +0300

    Added mongodb entity

---
+++ PersonMongoDbMinimalApi/Domain/Person.cs
@@ -0,0 +1,9 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace PersonMongoDbMinimalApi.Domain;
+public class Person
+{
+    [BsonId]
+    [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+    public string? Id { get; set; }
+}
