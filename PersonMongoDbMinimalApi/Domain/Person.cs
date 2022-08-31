7d85ae23b9b00ede4662799044b336efc8dac64b
Rostislav Fedoriv <froizerner@gmail.com>
2022-08-31 12:38:55 +0300

    Added athributes and props

--- PersonMongoDbMinimalApi/Domain/Person.cs
+++ PersonMongoDbMinimalApi/Domain/Person.cs
@@ -6,4 +6,19 @@ public class Person
     [BsonId]
     [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
     public string? Id { get; set; }
+
+    [BsonElement("firstName")]
+    public string? FirstName { get; set; }
+
+    [BsonElement("secondName")]
+    public string? SecondName { get; set; }
+
+    [BsonElement("age")]
+    public int Age { get; set; }
+
+    [BsonElement("email")]
+    public string? Email { get; set; }
+
+    [BsonElement("telephone")]
+    public string? Telephone { get; set; }
 }
