d818960a59e970f37dcaa9bf3883dd49cc5ee570
Rostislav Fedoriv <froizerner@gmail.com>
2022-08-31 11:20:11 +0300

    Added DBSettings

---
+++ PersonMongoDbMinimalApi/Database/IPersonDBSettings.cs
@@ -0,0 +1,7 @@
+﻿namespace PersonMongoDbMinimalApi.Database;
+public interface IPersonDBSettings
+{
+    public string PersonCollectionName { get; set; }
+    public string ConnectionString { get; set; }
+    public string DatabaseName { get; set; }
+}
---
+++ PersonMongoDbMinimalApi/Database/PersonDBSettings.cs
@@ -0,0 +1,7 @@
+﻿namespace PersonMongoDbMinimalApi.Database;
+public class PersonDBSettings : IPersonDBSettings
+{
+    public string PersonCollectionName { get; set; } = string.Empty;
+    public string ConnectionString { get; set; } = string.Empty;
+    public string DatabaseName { get; set; } = string.Empty;
+}
