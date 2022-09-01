960013ea490731886c1a7debbfbe4478bc9db75f
Rostislav Fedoriv <froizerner@gmail.com>
2022-09-01 22:02:38 +0300

    added mapping

---
+++ PersonMongoDbMinimalApi/Mapping/ApiContractToDomainMapper.cs
@@ -0,0 +1,32 @@
+﻿using PersonMongoDbMinimalApi.Contracts.Requests;
+using PersonMongoDbMinimalApi.Domain;
+
+namespace PersonMongoDbMinimalApi.Mapping;
+public static class ApiContractToDomainMapper
+{
+    public static Person ToPerson(this UpdatePersonRequest updatePersonRequest)
+    {
+        return new Person
+        {
+            Id = updatePersonRequest.Id,
+            FirstName = updatePersonRequest.FirstName,
+            SecondName = updatePersonRequest.SecondName,
+            Age = updatePersonRequest.Age,
+            Email = updatePersonRequest.Email,
+            Telephone = updatePersonRequest.Telephone,
+        };
+    }
+
+    public static Person ToPerson(this CreatePersonRequest updatePersonRequest)
+    {
+
+        return new Person
+        {
+            FirstName = updatePersonRequest.FirstName,
+            SecondName = updatePersonRequest.SecondName,
+            Age = updatePersonRequest.Age,
+            Email = updatePersonRequest.Email,
+            Telephone = updatePersonRequest.Telephone,
+        };
+    }
+}
---
+++ PersonMongoDbMinimalApi/Mapping/PersonDtoToPerson.cs
@@ -0,0 +1,33 @@
+﻿
+using PersonMongoDbMinimalApi.Contracts.DTO;
+using PersonMongoDbMinimalApi.Domain;
+
+namespace PersonMongoDbMinimalApi.Mapping;
+public static class PersonDtoToPerson
+{
+    public static Person ToPerson(this PersonDto dto)
+    {
+        return new Person 
+        {
+            Id = dto.Id,
+            Age = dto.Age,
+            FirstName = dto.FirstName,
+            SecondName = dto.SecondName,
+            Email = dto.Email,
+            Telephone = dto.Telephone
+        };
+    }
+    public static IEnumerable<Person> ToPeople(this List<PersonDto> dtos)
+    {
+        var people= dtos.Select(p => new Person
+        {
+            Id = p.Id,
+            Age = p.Age,
+            FirstName = p.FirstName,
+            SecondName = p.SecondName,
+            Email = p.Email,
+            Telephone = p.Telephone
+        });
+        return people;
+    }
+}
